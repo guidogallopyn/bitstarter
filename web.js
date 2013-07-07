2bbcfb740de3ca8e7bb57a0324271504ea84fdb3
Guido Gallopyn <guido.gallopyn@gmail.com>
2013-07-07 18:05:06 +0000

    bug fix attempt 3: fixed typo

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var buffer = fs.readFileSync("./index.html");
-  var page = buffer.tpString('utf-8');
+  var page = buffer.toString('utf-8');
   response.send(page);
 });
 
