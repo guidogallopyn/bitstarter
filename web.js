76e5edaff7ba34fc65862c0a4ad2281bb0efaf7e
Guido Gallopyn <guido.gallopyn@gmail.com>
2013-07-07 17:41:37 +0000

    bug fix attempt: fs.readFileSync encoding

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var page = fs.readFileSync("./index.html",{encoding: String});
+  var page = fs.readFileSync("./index.html",{encoding = 'utf8'});
   response.send(page);
 });
 
